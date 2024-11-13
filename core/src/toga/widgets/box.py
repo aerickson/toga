7c9a3571c769584a37adde880a768d28a71f65f2
Andrew Erickson <aerickson@gmail.com>
2024-11-13 03:46:22 -0800

    hack up layout debug mode
    when TOGA_DEBUG_LAYOUT env var is 1, box background will be alternated

--- core/src/toga/widgets/box.py
+++ core/src/toga/widgets/box.py
@@ -1,8 +1,22 @@
 from __future__ import annotations
 
+import os
+
 from collections.abc import Iterable
 
 from .base import StyleT, Widget
+
+
+# based on colors from https://davidmathlogic.com/colorblind
+ibm_pastel_palette = [
+    "#b8d2e9",  # light blue
+    "#f8ccb0",  # light orange
+    "#c7e7b2",  # light green
+    "#f0b2d6",  # light pink
+    "#e5dab0",  # light yellow
+    "#d5c2ea",  # light lavender
+    "#b2e4e5"   # light teal
+]
 
 
 class Box(Widget):
@@ -22,6 +36,20 @@ class Box(Widget):
             will be applied to the widget.
         :param children: An optional list of children for to add to the Box.
         """
+        # if layout debug mode, change bg color
+        if 'TOGA_DEBUG_LAYOUT' in os.environ and os.environ['TOGA_DEBUG_LAYOUT'] == '1':
+            # globals are gross, but ok when we're debugging
+            global color_index
+            try:
+                if color_index == len(ibm_pastel_palette) - 1:
+                    color_index = 0
+                else:
+                    color_index += 1
+            except NameError:
+                color_index = 0
+            selected_color = ibm_pastel_palette[color_index]
+            style.background_color = selected_color
+
         super().__init__(id=id, style=style)
 
         # Create a platform specific implementation of a Box
