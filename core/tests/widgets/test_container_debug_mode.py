70436872ff963da23e14f4fdcbdb749dd3b767e8
Andrew Erickson <aerickson@gmail.com>
2024-11-14 18:42:33 -0800

    improve test

--- core/tests/widgets/test_container_debug_mode.py
+++ core/tests/widgets/test_container_debug_mode.py
@@ -1,21 +1,15 @@
 from os import environ
 
 import toga
-from toga_dummy.utils import (
-    assert_action_not_performed,
-    assert_action_performed,
-)
 
 # Enable debug mode
 environ["TOGA_DEBUG_LAYOUT"] = "1"
 
 
-# clone of a test in test_box.py
-def test_create_box():
+def test_box_debug_background():
     """A Box can be created."""
     box = toga.Box()
-    # Round trip the impl/interface
-    assert box._impl.interface == box
 
-    assert_action_performed(box, "create Box")
-    assert_action_not_performed(box, "add child")
+    # assert that the bg is not default/white
+    assert hasattr(box.style, "background_color")
+    assert not box.style.background_color == toga.colors.rgb(0, 0, 0)
