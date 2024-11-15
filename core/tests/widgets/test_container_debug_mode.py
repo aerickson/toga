e9f9727c18f1e2cc3a43d0ce74b6ccd902d9c74b
Andrew Erickson <aerickson@gmail.com>
2024-11-14 19:02:06 -0800

    test the bg on a normal box

--- core/tests/widgets/test_container_debug_mode.py
+++ core/tests/widgets/test_container_debug_mode.py
@@ -1,15 +1,29 @@
 from os import environ
 
-import toga
+from travertino.colors import rgb
 
-# Enable debug mode
-environ["TOGA_DEBUG_LAYOUT"] = "1"
+import toga
 
 
 def test_box_debug_background():
-    """A Box can be created."""
+    """A Box in layout debug mode has a non-default background."""
+    # Enable layout debug mode
+    environ["TOGA_DEBUG_LAYOUT"] = "1"
+
     box = toga.Box()
 
     # assert that the bg is not default/white
     assert hasattr(box.style, "background_color")
-    assert not box.style.background_color == toga.colors.rgb(0, 0, 0)
+    assert not box.style.background_color == rgb(0, 0, 0)
+
+
+def test_box_normal_background():
+    """A Box has no default background."""
+    # Disable layout debug mode
+    environ["TOGA_DEBUG_LAYOUT"] = "0"
+
+    box = toga.Box()
+
+    # assert that the bg is not default/white
+    assert hasattr(box.style, "background_color")
+    assert not box.style.background_color
