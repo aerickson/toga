d1c333d7f48f74d17a1967ad44d60f55b463c6bf
Andrew Erickson <aerickson@gmail.com>
2024-12-04 00:29:45 -0600

    test_container_debug_mode.py::test_box_debug_background: test for actual colors

--- core/tests/widgets/test_container_debug_mode.py
+++ core/tests/widgets/test_container_debug_mode.py
@@ -1,6 +1,5 @@
-from os import environ
-
-from travertino.colors import rgb
+from pytest import MonkeyPatch
+from travertino.colors import color
 
 import toga
 
@@ -9,49 +8,43 @@ import toga
 def test_button_debug_background():
     """A Button in layout debug mode has a default background."""
     # Enable layout debug mode
-    environ["TOGA_DEBUG_LAYOUT"] = "1"
-
-    button = toga.Button()
-
-    # assert that the bg is default
-    assert hasattr(button.style, "background_color")
-    assert not button.style.background_color
+    with MonkeyPatch.context() as mp:
+        mp.setenv("TOGA_DEBUG_LAYOUT", "1")
+        button = toga.Button()
+        # assert that the bg is default
+        assert hasattr(button.style, "background_color")
+        assert not button.style.background_color
 
 
 # test that a container-like widget in layout debug mode has a non-default background
 def test_box_debug_background():
     """A Box in layout debug mode has a non-default background."""
     # Enable layout debug mode
-    environ["TOGA_DEBUG_LAYOUT"] = "1"
+    with MonkeyPatch.context() as mp:
+        mp.setenv("TOGA_DEBUG_LAYOUT", "1")
 
-    # need enough for coverage of palette array index rollover
-    box = toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
-    toga.Box()
+        boxes = []
+        debug_bg_palette_length = len(toga.widgets.base.debug_background_palette)
+        # need enough for coverage of debug_background_palette array index rollover
+        for i in range(debug_bg_palette_length + 3):
+            boxes.append(toga.Box())
 
-    # assert that the bg is not default/white
-    assert hasattr(box.style, "background_color")
-    assert not box.style.background_color == rgb(0, 0, 0)
+        for counter, box in enumerate(boxes, start=1):
+            index = counter % debug_bg_palette_length
+            print(counter)
+            assert hasattr(box.style, "background_color")
+            assert box.style.background_color == color(
+                toga.widgets.base.debug_background_palette[index]
+            )
 
 
 # test that a container-like widget in normal mode has a default background
 def test_box_normal_background():
     """A Box has no default background."""
     # Disable layout debug mode
-    environ["TOGA_DEBUG_LAYOUT"] = "0"
-
-    box = toga.Box()
-
-    # assert that the bg is default
-    assert hasattr(box.style, "background_color")
-    assert not box.style.background_color
+    with MonkeyPatch.context() as mp:
+        mp.setenv("TOGA_DEBUG_LAYOUT", "1")
+        box = toga.Box()
+        # assert that the bg is default
+        assert hasattr(box.style, "background_color")
+        assert not box.style.background_color
